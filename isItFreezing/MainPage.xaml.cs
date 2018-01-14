d08d12840abaa00193bad025382ab97ed15bd73e
Dylan Z <dtzitz@live.com>
2018-01-14 18:26:56 -0500

    probably should have made an earlier commit

--- isItFreezing/MainPage.xaml.cs
+++ isItFreezing/MainPage.xaml.cs
@@ -3,15 +3,20 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.ApplicationModel.Background;
+using Windows.Devices.Gpio;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Windows.System.Threading;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
 using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Media.Imaging;
 using Windows.UI.Xaml.Navigation;
+using static isItFreezing.OpenWeatherProxy;
 
 // The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
 
@@ -25,6 +30,153 @@ namespace isItFreezing
         public MainPage()
         {
             this.InitializeComponent();
+            InitGPIO();
+            
         }
+
+        private int DELETEME = 35;
+
+        private const int LED_PIN = 5;
+        public GpioPin pin;
+        private GpioPinValue pinValue;
+        private bool isLightOn;
+
+        private void InitGPIO()
+        {
+            var gpio = GpioController.GetDefault();
+
+            // Show an error if there is no GPIO controller
+            if (gpio == null)
+            {
+                //uhh
+                return;
+            }
+
+            pin = gpio.OpenPin(LED_PIN);
+            pinValue = GpioPinValue.High;
+            pin.Write(pinValue);
+            pin.SetDriveMode(GpioPinDriveMode.Output);
+
+        }
+
+
+        private async void Florida_Loaded(object sender, RoutedEventArgs e)
+        {
+            int zipcode = 33709;
+            RootObject myWeather = await OpenWeatherMapProxy.GetWeatherAsync(zipcode);
+
+            //make the background
+            string flWeatherIcon = String.Format("ms-appx:///Assets/Florida/{0}.png", myWeather.weather[0].icon);
+            ImageBrush ib = new ImageBrush();
+            ib.ImageSource = new BitmapImage(new Uri(flWeatherIcon, UriKind.Absolute));
+            ib.Stretch = Stretch.UniformToFill;
+            Florida.Background = ib;
+
+            //weather info
+            flName.Text = myWeather.name;
+            flTemp.Text = "Temp: " + ((int)myWeather.main.temp).ToString()+ " F";
+            flHumidity.Text = "Humidity: " + (myWeather.main.humidity).ToString();
+            flCondition.Text = myWeather.weather[0].description;
+
+            //Florida Timer
+            TimeSpan period = TimeSpan.FromMinutes(5);
+            ThreadPoolTimer PerodicTimer = ThreadPoolTimer.CreatePeriodicTimer(async (Florida_Loaded) => {
+                myWeather = await OpenWeatherMapProxy.GetWeatherAsync(zipcode);
+                
+                if ((int)myWeather.main.temp <= 32 && pinValue == GpioPinValue.High)
+                {
+                    pinValue = GpioPinValue.Low;
+                    pin.Write(pinValue);
+                }
+                if ((int)myWeather.main.temp > 32 && pinValue == GpioPinValue.Low)
+                {
+                    
+                    pinValue = GpioPinValue.High;
+                    pin.Write(pinValue);
+                }
+                
+
+
+#pragma warning disable CS4014 // Because this call is not awaited, execution of the current method continues before the call is completed
+                Dispatcher.RunAsync(Windows.UI.Core.CoreDispatcherPriority.High, () => {
+
+
+                    //make the background
+                    flWeatherIcon = String.Format("ms-appx:///Assets/Florida/{0}.png", myWeather.weather[0].icon);
+                    ib.ImageSource = new BitmapImage(new Uri(flWeatherIcon, UriKind.Absolute));
+                    ib.Stretch = Stretch.UniformToFill;
+                    Florida.Background = ib;
+
+                    //weather info
+                    flName.Text = myWeather.name;
+                    flTemp.Text = "Temp: " + ((int)myWeather.main.temp).ToString() + " F";
+                    flHumidity.Text = "Humidity: " + (myWeather.main.humidity).ToString();
+                    flCondition.Text = myWeather.weather[0].description;
+                    flTimeUpdated.Text = "Last Updated on: " + DateTime.Now.ToShortTimeString();
+
+                });
+#pragma warning restore CS4014 // Because this call is not awaited, execution of the current method continues before the call is completed
+            }, period);
+
+
+        }
+
+        private async void Colorado_Loaded(object sender, RoutedEventArgs e)
+        {
+            int zipcode = 80918;
+            RootObject coWeather = await OpenWeatherMapProxy.GetWeatherAsync(zipcode);
+
+            //make the background
+            string coWeatherIcon = String.Format("ms-appx:///Assets/Colorado/{0}.png", coWeather.weather[0].icon);
+            ImageBrush ib = new ImageBrush();
+            ib.ImageSource = new BitmapImage(new Uri(coWeatherIcon, UriKind.Absolute));
+            ib.Stretch = Stretch.UniformToFill;
+            Colorado.Background = ib;
+
+            //weather info
+            coName.Text = coWeather.name;
+            coTemp.Text = "Temp: " + ((int)coWeather.main.temp).ToString() + " F";
+            coHumidity.Text = "Humidity: " + (coWeather.main.humidity).ToString();
+            coCondition.Text = coWeather.weather[0].description;
+
+            //Refresh timer
+            TimeSpan period = TimeSpan.FromMinutes(5);
+            ThreadPoolTimer PerodicTimer = ThreadPoolTimer.CreatePeriodicTimer(async (Colorado_loaded) => {
+                coWeather = await OpenWeatherMapProxy.GetWeatherAsync(zipcode);
+
+                if ((int)coWeather.main.temp <= 32 && pinValue == GpioPinValue.High)
+                {
+                    pinValue = GpioPinValue.Low;
+                    pin.Write(pinValue);
+                }
+                if ((int)coWeather.main.temp > 32 && pinValue == GpioPinValue.Low)
+                {
+
+                    pinValue = GpioPinValue.High;
+                    pin.Write(pinValue);
+                }
+
+#pragma warning disable CS4014 // Because this call is not awaited, execution of the current method continues before the call is completed
+                Dispatcher.RunAsync(Windows.UI.Core.CoreDispatcherPriority.High, () => {
+
+                    //make the background
+                    coWeatherIcon = String.Format("ms-appx:///Assets/Colorado/{0}.png", coWeather.weather[0].icon);
+                    ib.ImageSource = new BitmapImage(new Uri(coWeatherIcon, UriKind.Absolute));
+                    ib.Stretch = Stretch.UniformToFill;
+                    Colorado.Background = ib;
+
+                    //weather info
+                    coName.Text = coWeather.name;
+                    coTemp.Text = "Temp: " + ((int)coWeather.main.temp).ToString() + " F";
+                    coHumidity.Text = "Humidity: " + (coWeather.main.humidity).ToString();
+                    coCondition.Text = coWeather.weather[0].description;
+                    coTimeUpdated.Text = "Last Updated on: " + DateTime.Now.ToShortTimeString() + " | Colorado Time: " + DateTime.Now.AddHours(-2).ToShortTimeString();
+
+                });
+#pragma warning restore CS4014 // Because this call is not awaited, execution of the current method continues before the call is completed
+            }, period);
+        }
+
+
     }
 }
---
+++ isItFreezing/OpenWeatherProxy.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Json;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace isItFreezing
+{
+    class OpenWeatherProxy
+    {
+        public class OpenWeatherMapProxy
+        {
+            public static async Task<RootObject> GetWeatherAsync(int zipcode)
+            {
+                var http = new HttpClient();
+                string weatherUrl = String.Format("http://api.openweathermap.org/data/2.5/weather?zip={0},us&appid=1597aa818dd8c98bd122399d4e92dc85&units=imperial", zipcode);
+                var response = await http.GetAsync(weatherUrl);
+                var result = await response.Content.ReadAsStringAsync();
+                var seralizer = new DataContractJsonSerializer(typeof(RootObject));
+                var ms = new MemoryStream(Encoding.UTF8.GetBytes(result));
+
+                var data = (RootObject)seralizer.ReadObject(ms);
+                return data;
+            }
+        }
+        [DataContract]
+        public class Coord
+        {
+            [DataMember]
+            public double lon { get; set; }
+
+            [DataMember]
+            public double lat { get; set; }
+        }
+
+        [DataContract]
+        public class Weather
+        {
+            [DataMember]
+            public int id { get; set; }
+
+            [DataMember]
+            public string main { get; set; }
+
+            [DataMember]
+            public string description { get; set; }
+
+            [DataMember]
+            public string icon { get; set; }
+        }
+
+        [DataContract]
+        public class Main
+        {
+            [DataMember]
+            public double temp { get; set; }
+
+            [DataMember]
+            public int pressure { get; set; }
+
+            [DataMember]
+            public int humidity { get; set; }
+
+            [DataMember]
+            public double temp_min { get; set; }
+
+            [DataMember]
+            public double temp_max { get; set; }
+        }
+
+        [DataContract]
+        public class Wind
+        {
+            [DataMember]
+            public double speed { get; set; }
+
+            [DataMember]
+            public double deg { get; set; }
+        }
+
+        [DataContract]
+        public class Clouds
+        {
+            [DataMember]
+            public int all { get; set; }
+        }
+
+        [DataContract]
+        public class Sys
+        {
+            [DataMember]
+            public int type { get; set; }
+
+            [DataMember]
+            public int id { get; set; }
+
+            [DataMember]
+            public double message { get; set; }
+
+            [DataMember]
+            public string country { get; set; }
+
+            [DataMember]
+            public int sunrise { get; set; }
+
+            [DataMember]
+            public int sunset { get; set; }
+        }
+
+        [DataContract]
+        public class RootObject
+        {
+            [DataMember]
+            public Coord coord { get; set; }
+
+            [DataMember]
+            public List<Weather> weather { get; set; }
+
+            [DataMember]
+            public string @base { get; set; }
+
+            [DataMember]
+            public Main main { get; set; }
+
+            [DataMember]
+            public int visibility { get; set; }
+
+            [DataMember]
+            public Wind wind { get; set; }
+
+            [DataMember]
+            public Clouds clouds { get; set; }
+
+            [DataMember]
+            public int dt { get; set; }
+
+            [DataMember]
+            public Sys sys { get; set; }
+
+            [DataMember]
+            public int id { get; set; }
+
+            [DataMember]
+            public string name { get; set; }
+
+            [DataMember]
+            public int cod { get; set; }
+        }
+    }
+}
